7f84f45a59e4c7951b89713216a0bd9e1cd074c5
Raneen Asia <Raneenasia101@gmail.com>
2023-03-28 09:49:26 +0300

    Create DoctorWhoCoreDbContext

---
+++ DoctorWho/DoctorWho.Db/DoctorWhoDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace DoctorWho.Db
+{
+    public class DoctorWhoDbContext : DbContext
+    {
+        public DoctorWhoDbContext()
+        {
+        }
+
+        public DoctorWhoDbContext(DbContextOptions<DoctorWhoDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(
+                    "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = DoctorWhoCore")
+                    .LogTo(Console.WriteLine, new[]
+                    {DbLoggerCategory.Database.Command.Name },
+                     LogLevel.Information).EnableSensitiveDataLogging();
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
